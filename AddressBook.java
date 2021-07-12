e64056bfa4f4b6fcd357f6277aa81fe97cf76d89
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-13 00:34:58 +0530

    added:mutiple adressBooks can be done

--- AddressBook.java
+++ AddressBook.java
@@ -1,129 +1,99 @@
-package adressBook;
-
-
-public class Contacts {
-    //instance variables
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String city;
-    private String state;
-    private String emailId;
-    private long zip;
-    private long phoneNumber;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
-    public long getZip() {
-        return zip;
-    }
-
-    public void setZip(long zip) {
-        this.zip = zip;
-    }
-
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Override
-    public String toString() {
-        return "Contacts{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", emailId='" + emailId + '\'' +
-                ", zip=" + zip +
-                ", phoneNumber=" + phoneNumber +
-                '}';
-    }
-}
-
 package com.adressbook;
 import java.util.*;
 import java.util.Scanner;
 
 public class AddressBook
 {
+	//adding contact class to a arrayList
 	public static ArrayList<Contacts> contacts=new ArrayList<>();
+	//initializing an dictionary for initializing separate arraylist and hashtable to store each separate adressBooks
+	public static Dictionary<String,ArrayList<Contacts>> dict= new Hashtable<String,ArrayList<Contacts>>();
 	static Scanner sc=new Scanner(System.in);
 	public static void main(String args[])
 	{
 		boolean options = true;
-		while (options) {
-			System.out.println("Enter \n 1)To Add contacts \n 2) To edit contacts\n 3)To Delete Contacts\n 4)To Exit");
+		while(options){
+			System.out.println("Enter \n 1)To add new Address Book \n 2)Add Contacts to old Address Book\n 3)To display all AdressBooks\n 4.)To Exit");
 			System.out.println("Enter the option : ");
-			int option = sc.nextInt();
-			switch (option) {
-			case 1:
-				AddressBook.addContacts();
+			int choice = sc.nextInt();
+			if(choice==4)
+			{
 				break;
-			case 2:
-				AddressBook.editContacts();
-				break;
-			case 3:
-				AddressBook.deleteContacts();
-				break;
-			default:
-				System.out.println("Invalid Option");
 			}
+			switch(choice){
+			case 1:System.out.println("Enter the name of Adress Book:");
+			String name=sc.next();
+			//assigning all contacts to this particular AdressBook
+			ArrayList<Contacts> tempContacts=new ArrayList<>();
+			contacts=tempContacts;
+			while (options) {
+				System.out.println("Enter \n 1)To Add contacts \n 2) To edit contacts\n 3)To Delete Contacts\n 4)To Exit");
+				System.out.println("Enter the option : ");
+				int option = sc.nextInt();
+				if(option==4)
+				{
+					break;
+				}
+				switch (option) {
+				case 1:
+					AddressBook.addContacts();
+					break;
+				case 2:
+					AddressBook.editContacts();
+					break;
+				case 3:
+					AddressBook.deleteContacts();
+					break;
+				default:
+					System.out.println("Invalid Option");
+				}
+			}
+			dict.put(name,contacts);
+			break;
+			case 2:System.out.println("Enter the name of Address book in which you want to insert:");
+			name=sc.next();
+			ArrayList<Contacts> tempContacts1=new ArrayList<>();
+			contacts=tempContacts1;
+			contacts=dict.get(name);
+			while (options) {
+				System.out.println("Enter \n 1)To Add contacts \n 2) To edit contacts\n 3)To Delete Contacts\n 4)To Exit");
+				System.out.println("Enter the option : ");
+				int option = sc.nextInt();
+				if(option==4)
+				{
+					break;
+				}
+				switch (option) {
+				case 1:
+					AddressBook.addContacts();
+					break;
+				case 2:
+					AddressBook.editContacts();
+					break;
+				case 3:
+					AddressBook.deleteContacts();
+					break;
+				default:
+					System.out.println("Invalid Option");
+					break;
+				}
+				dict.put(name,contacts);
 
+			}
+			break;
+			case 3:
+				System.out.println(dict);
+				break;
+
+			default:System.out.println("Wrong Value");
+			break;
+
+			}
 		}
+
 	}
 
-
+	//method to add multiple contacts
 	static void addContacts()
 	{
 		int choice=0;
@@ -160,22 +130,19 @@ public class AddressBook
 			else
 			{
 				choice=1;
-				System.out.println(contacts);
-
 			}
-
 		}
 	}
-
+	//method to edit contact using their first names
 	static void editContacts() 
 	{
-		System.out.println("Enter firstname of the user you want to the edit:");
+		System.out.println("Enter firstname of the user to whome you want to edit:");
 		String firstName = sc.next();
 		for (Contacts c : contacts) {
 			if (c.getFirstName().equals(firstName)) {
 
 				System.out.println("c");
-				System.out.println("Enter the  field which u want to edit:");
+				System.out.println("Enter the type which u want to edit:");
 
 				System.out.println(" Address");
 				System.out.println(" City ");
@@ -184,32 +151,31 @@ public class AddressBook
 				System.out.println(" PhoneNumber");
 				System.out.println(" ZipCode");
 				System.out.println("Exit");
-				String field = sc.next();
+				String type = sc.next();
 
-				if (field.equals("firstName")) {
+				if (type.equals("firstName")) {
 					c.setFirstName(sc.next());
-				} else if (field.equals("lastName")) {
+				} else if (type.equals("lastName")) {
 					c.setLastName(sc.next());
-				} else if (field.equals("Address")) {
+				} else if (type.equals("Address")) {
 					c.setAdress(sc.next());
-				} else if (field.equals("City")) {
+				} else if (type.equals("City")) {
 					c.setCity(sc.next());
-				} else if (field.equals("State")) {
+				} else if (type.equals("State")) {
 					c.setState(sc.next());
-				} else if (field.equals("Email")) {
+				} else if (type.equals("Email")) {
 					c.setEmail(sc.next());
-				} else if (field.equals("Zip")) {
+				} else if (type.equals("Zip")) {
 					c.setZip(sc.nextInt());
-				} else if (field.equals("phoneNumber")) {
+				} else if (type.equals("phoneNumber")) {
 					c.setPhoneNo(sc.nextLong());
 				}
 			}
-
 		}
 		System.out.println(contacts.toString());
 
 	}
-
+	//method to delete a contact using firstName
 	static void deleteContacts()
 	{
 		System.out.println("Enter firstname of the user you want to delete:");
@@ -224,4 +190,5 @@ public class AddressBook
 			System.out.println(contacts);
 		}
 	}
+
 }
